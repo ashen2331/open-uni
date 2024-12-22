eb5423dea68efc58c50039387f762a2d39b9b106
ashen2331 <120397128+ashen2331@users.noreply.github.com>
2024-12-22 18:49:08 +0530

    Add files via upload

---
+++ repeate me.py
@@ -0,0 +1,10 @@
+def repeat_text(text, number_of_repetitions):
+ 
+  return text * number_of_repetitions
+
+
+text = "hello"
+repetitions = 10
+
+repeated_text = repeat_text(text, repetitions)
+print(repeated_text)
